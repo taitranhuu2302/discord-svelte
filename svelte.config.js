60819cc8bb66662f992acc7a79a94ba49f9fe4a0
Tai Tran <tai.tran@mgisolutions.com>
2023-08-28 11:32:11 +0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,22 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import {vitePreprocess} from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	base: '/discord-svelte',
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    base: process.env.NODE_ENV === 'production' ? '/discord-svelte' : '',
+    preprocess: vitePreprocess(),
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter({
+            pages: "build",
+            assets: "build",
+            fallback: null,
+        })
+    }
 };
 
 export default config;
