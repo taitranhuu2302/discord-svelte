a3fd3d8fe5b793cd3377ddab5ebcded4dc19ed0a
Tai Tran <tai.tran@mgisolutions.com>
2023-08-28 11:14:46 +0700

    update

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -4,7 +4,8 @@
 	export let hoverTextColor: string = ""
 </script>
 
-<div class="btn" hover-tooltip={tooltip} style={`--btn-hover-color: ${hoverColor}; --btn-hover-text-color: ${hoverTextColor}`}>
+<div class="btn" hover-tooltip={tooltip}
+     style={`--btn-hover-color: ${hoverColor}; --btn-hover-text-color: ${hoverTextColor}`}>
 	<button>
 		<slot name="icon"/>
 	</button>
@@ -16,7 +17,7 @@
 
         &:after {
             content: attr(hover-tooltip);
-            @apply text-center absolute whitespace-nowrap font-semibold left-[calc(100%+10px)] px-[12px] py-[4px] rounded-[6px] opacity-0 invisible transition-all bg-[#222127];
+            @apply text-center absolute whitespace-nowrap font-semibold left-[calc(100%+10px)] px-[14px] py-[4px] rounded-[6px] opacity-0 invisible transition-all bg-[#222127];
         }
 
         &:hover {
@@ -51,11 +52,37 @@
     }
 
     .btn:hover button {
-        @apply rounded-[12px] text-white transition-all duration-100;
+        @apply text-white transition-all duration-100 rounded-[14px];
         background: var(--btn-hover-color);
+        animation: borders 0.3s linear;
     }
 
     button {
         @apply w-[48px] relative h-[48px] overflow-hidden rounded-full bg-[#313338] flex items-center justify-center transition-all duration-100;
+        animation: defaultBorder 0.3s linear;
+    }
+
+    @keyframes defaultBorder {
+        0% {
+            @apply rounded-[14px];
+        }
+        50% {
+            @apply rounded-[30px];
+        }
+        100% {
+            @apply rounded-full;
+        }
+    }
+
+    @keyframes borders {
+        0% {
+            @apply rounded-full;
+        }
+        50% {
+            @apply rounded-[30px];
+        }
+        100% {
+            @apply rounded-[14px];
+        }
     }
 </style>
